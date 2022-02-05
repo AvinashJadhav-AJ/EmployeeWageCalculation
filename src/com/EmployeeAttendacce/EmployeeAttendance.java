bd5ab922e350a81232d789e593da38d9e03a7c4d
Avinash <aj912873@gmail.com>
2022-02-05 23:47:33 +0530

    Finding Employee Attendace

---
+++ src/com/EmployeeAttendacce/EmployeeAttendance.java
@@ -0,0 +1,12 @@
+package com.EmployeeAttendacce;
+
+public class EmployeeAttendance {
+	public static void main(String[] args) {
+	int is_full_time = 1;
+	double empcheck = Math.floor(Math.random() * 100) % 2;
+	if ( empcheck == is_full_time )
+		System.out.println("Employee is Presnet");
+	else
+		System.out.println("Employee is Absent");
+	}
+}
