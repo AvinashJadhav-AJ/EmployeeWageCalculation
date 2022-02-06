696a81d7aff3f2936c0ea21d548cbd9e9e56855e
Avinash <aj912873@gmail.com>
2022-02-06 15:32:43 +0530

    Added: Employee Monthly Wage

---
+++ EmployeeMonthlyWage/src/com/employeemonthlywage/employeemonthlywage.java
@@ -0,0 +1,40 @@
+package com.employeemonthlywage;
+
+public class employeemonthlywage {
+	
+	public static final int is_full_time = 1;
+	public static final int is_part_time = 2;
+	public static final int Employee_wage_per_Hour = 20;
+	public static final int number_of_working_day = 20;
+	
+	public static void main(String[] args) {
+		
+		int empHrs = 0;
+		int empwage = 0;
+		int totalempwage = 0;
+		
+		for (int day = 0; day < number_of_working_day; day++) {
+		int empcheck = (int) Math.floor(Math.random() * 100) % 3;
+		switch (empcheck) {
+		
+			case is_full_time:
+				empHrs = 8;
+				break;
+		
+			case is_part_time:
+		    	empHrs = 4;
+		    	break;
+		
+			default:
+				empHrs = 0;
+				break;
+			}
+		empwage = empHrs * Employee_wage_per_Hour;
+		totalempwage += empwage;
+		    System.out.println("empwage: " + empwage);
+		}
+		System.out.println("Total Emp Wage: " + totalempwage );
+		    		
+	}
+
+}
